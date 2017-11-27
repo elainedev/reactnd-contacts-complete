ff3bf12dcc7ff06740c5a8e1eec07036b7932690
Elaine Wang <elainewang.cal@gmail.com>
2017-11-26 23:42:08 -0800

    commit ListContacts and server files

---
+++ src/ListContacts.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class ListContacts extends Component {
+  // style is a prop
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contactsProp.map(contact => (
+          <li key={contact.id} className='contact-list-item'>
+            <div className='contact-avatar' style={
+                {
+                  backgroundImage: `url(${contact.avatarURL})`
+                }
+              }/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
+
+}
+
+export default ListContacts
